14cc53729e1bc8da806f59c43db9b77743ee69df
guillaume.le.berre@student.howest.be <guillaume.le.berre@student.howest.be>
2023-05-03 09:16:21 +0200

    Had to change the snakemake file to make sure the order of rules is respected and not executed in parallel

--- Snakefile
+++ Snakefile
@@ -1,12 +1,13 @@
 # Rule to get all the output files and save
 rule all:
     input:
-        "reports/Results.html",
-        "reports/Statistics.txt",
-        directory("ProwlerProcessed")
-
-# The rule to call the porechop program and saving the output in files
-rule porechopABI_call:
+        #"PorechopABI/Output_reads.fastq",
+        #"reports/Statistics.txt",
+        #directory("ProwlerProcessed"),
+        #"SACRAResults/sacra_results.fasta",
+        "reports/Results.html"
+# Rule to call the porechop program and saving the output in files
+rule porechopABIcall:
     input: 
         "PorechopABI/fastq_runid_211de24bb98b581ec357aee6dd1409fc7b321927_0_0.fastq"
     output: 
@@ -17,20 +18,35 @@ rule porechopABI_call:
     shell: 
         "porechop_abi -abi -i {input} -o {output.reads} > {output.statistics} 2>&1"
         
+# Rule to trim the reads using Prowler and save the output
+# Define the script folder in the rule
+rule ProwlerTrim:
+    input:
+        out_fastq="PorechopABI/Output_reads.fastq" 
+    output:
+        out_prow="ProwlerProcessed/Output_readsTrimLT-U0-D7W100L100R0.fasta"
+    shell:
+        "python3 scripts/TrimmerLarge.py -f {input.out_fastq} -i PorechopABI/ -o ProwlerProcessed -m 'D' -r '.fasta'"
+
+#Rule to use the SACRA reads
+rule SACRAcall:
+    input:
+        in_sacra="ProwlerProcessed/Output_readsTrimLT-U0-D7W100L100R0.fasta"
+    output: 
+        "SACRAResults/sacra_results.fasta"
+    conda:
+        "envs/sacra.yaml"
+    shell: 
+        "scripts/SACRA.sh -i {input.in_sacra} -p {output} -t 6 -c config_sacra.yml"
+
 # Get the output from the terminal into an html file which can be used to generate the report. 
 rule StatisticsToHTML:
     input: 
-        "reports/Statistics.txt"
+        pore="reports/Statistics.txt",
+        prow1="PorechopABI/Output_reads.fastq",
+        prow2="ProwlerProcessed/Output_readsTrimLT-U0-D7W100L100R0.fasta",
+        sacra="SACRAResults/sacra_results.fasta"
     output: 
         "reports/Results.html"
     script: 
         "scripts/generate_report.py"
-
-# Rule to trim the reads using Prowler and save the output
-rule ProwlerTrim:
-    input:
-        "PorechopABI/Output_reads.fastq" 
-    output:
-        directory("ProwlerProcessed") 
-    shell:
-        "python3 scripts/TrimmerLarge.py -f {input} -i PorechopABI/ -o {output} -m 'D' -r '.fasta'"
--- scripts/generate_report.py
+++ scripts/generate_report.py
@@ -3,6 +3,9 @@
 
 #Importing needed modules
 import re, os, webbrowser, time
+from Bio import SeqIO   # pip install biopython
+import numpy as np
+import matplotlib.pyplot as plt     # pip install matplotlib
 
 # Change the path to the directory of the file
 #os.chdir("../reports/")
@@ -57,40 +60,88 @@ with open("reports/Statistics.txt", "r") as text_file:
     print(cleaned_text_list)      
 
 #######################################
-# GENERATIN HTML REPORT
+# GENERATING PROWLER REPORT
+#######################################
+
+# Lists to store the read lengths before and after trimming
+before_trim = []
+after_trim = []
+
+# Read the input fastq file and append the length of each read to before_trim
+for seq_record in SeqIO.parse("PorechopABI/Output_reads.fastq", "fastq"):
+    before_trim.append(len(seq_record.seq))
+
+# Read the trimmed fasta file and append the length of each read to after_trim
+for seq_record in SeqIO.parse("ProwlerProcessed/Output_readsTrimLT-U0-D7W100L100R0.fasta", "fasta"):
+    after_trim.append(len(seq_record.seq))
+
+# Convert the read length lists to numpy arrays for plotting
+before_array = np.array(before_trim)
+after_array = np.array(after_trim)
+
+# Create a figure with two subplots
+fig, axs = plt.subplots(1, 2, tight_layout=True)
+
+# Plot a histogram of read lengths before trimming
+axs[0].hist(before_array, bins=30, range=[0,10000])
+axs[0].set_title('Before trimming Reads')
+axs[0].set_xlabel('Read length')
+axs[0].set_ylabel('Frequency')
+
+# Plot a histogram of read lengths after trimming
+axs[1].hist(after_array, bins=30, range=[0,10000])
+axs[1].set_title('After trimming Reads')
+axs[1].set_xlabel('Read length')
+axs[1].set_ylabel('Frequency')
+
+# Saving the file before show
+plt.savefig("reports/Before&After-Prowler.png", dpi=200)
+
+
+#######################################
+# GENERATING HTML REPORT
 #######################################
 
 # Creating the header line
-html_header = f"<html>  <head> </head> <body> <h1> {cleaned_text_list[0]} </h1>"
+html_header = f"<html>\n<head>\n</head>\n<body>\n<h1>Statistical Report - Workflow</h1>\n<h2>Porechop ABI</h2>\n<h3>{cleaned_text_list[0]}</h3>\n"
 
 # Creating the table containg everything
 
 # The last 3 are summary lines from the adapters trimmed so can parse everything until then
 unorder_list = []
-unorder_list.append("<ul>")
+unorder_list.append("<ul>\n")
 for adapter in cleaned_text_list[1:-3]:
 
-    unorder_list.append(f"<li>{adapter}</li>")
-unorder_list.append("</ul>")
+    unorder_list.append(f"\t<li>{adapter}</li>\n")
+unorder_list.append("</ul>\n")
 #print(unorder_list)
-
+# Header adapter loc
+adap_loc_header = "<h3>Adapters Removed</h3>\n"
 # Adding the last summary lines from where the adapters are removed or not. 
 adapter_loc_list = []
 for adap_loc in cleaned_text_list[-3:]:
-    adapter_loc_list.append(f"<div>{adap_loc}</div>")
+
+    adapter_loc_list.append(f"<div>\n\t{adap_loc}\n</div>\n")
 
 # Adding the last line to the file
-html_end = "</body> </html>"
+html_end = "</body>\n</html>"
 
 with open("reports/Results.html", "w") as html_file:
+    # Porechop ABI
     html_file.writelines(html_header)
-    
+    # Adapters
     for ul in unorder_list:
         html_file.writelines(ul)
     
+    html_file.writelines(adap_loc_header)
+    #Removed adapters
     for summary in adapter_loc_list:
         html_file.writelines(summary)
-    
+    # PROWLER
+    html_file.writelines("<h2>Prowler Trimming</h2>\n")
+    # Have to set the locatio from where the html file will be, so set picture in same folder
+    html_file.writelines("\t<img src='Before&After-Prowler.png' height='800px'>\n")
+
     html_file.writelines(html_end)
 
 # Does not work on the WSL ubuntu yet, could be because no browser installed on it
